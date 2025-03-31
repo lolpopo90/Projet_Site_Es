512e1ef35fe179b7d6325e270f1038f2421a3dac
lolpopo90 <kirikuaS3@gmail.com>
2025-03-31 13:56:16 +0200

    Delete !!site directory

--- !!site/Java.js
+++
@@ -1,4 +0,0 @@
-function changerTexte() {
-    const texte = document.getElementById("explications");
-    texte.textContent = "La station météo est équipée de capteurs performants et utilise un Arduino pour la collecte des données.";
-}
