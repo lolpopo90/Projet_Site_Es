2f7f950fc61fdd91e4f9727148c3012514729154
lolpopo90 <kirikuaS3@gmail.com>
2025-03-31 13:58:03 +0200

    Add files via upload

---
+++ Java.js
@@ -0,0 +1,4 @@
+function changerTexte() {
+    const texte = document.getElementById("explications");
+    texte.textContent = "La station météo est équipée de capteurs performants et utilise un Arduino pour la collecte des données.";
+}
